94b36f4965889e75e0251ee7622e5c0f790620b8
Safeya-Yasien <ssafeya5@gmail.com>
2026-04-20 23:16:44 +0200

    feat: setup layout with navbar, footer, and routing navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { AddTask } from './pages/add-task/add-task';
+import { TaskDetails } from './pages/task-details/task-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'add-task',
+    component: AddTask,
+  },
+  {
+    path: 'task/:id',
+    component: TaskDetails,
+  },
+];
